e594e25d8ff598d063be390642b6f76f3f33a65d
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2020-09-18 16:49:39 +0530

    retrieve wallet transaction history

--- paytm.js
+++ paytm.js
@@ -1,9 +1,19 @@
 const auth = require('./lib/auth')
+const walletHistory = require('./lib/history')
 
 class Paytm {
-    constructor(number, password) {
-        this.number = number
-        this.password = password
+    constructor(payload) {
+        console.log(payload)
+        this.access = {
+            accessToken: null,
+            expiresAt: null
+        }
+        if (payload.accessToken) {
+            this.access.accessToken = payload.accessToken
+        } else {
+            this.number = payload.number
+            this.password = payload.password
+        }
     }
 
     async login() {
@@ -14,13 +24,17 @@ class Paytm {
     async verifyOtp(otp) {
         this.oauthToken = await auth.verifyOtp(otp, this.stateToken)
         this.access = await auth.getAccessToken(this.oauthToken)
+        return this.access.accessToken
     }
     
     async getAccessToken() {
         return this.access.accessToken
     }
 
-    
+    async getWalletHistory(offset=0, limit=20) {
+        return await walletHistory(this.access.accessToken, offset, limit)
+    }
+
 }
 
 module.exports = Paytm
